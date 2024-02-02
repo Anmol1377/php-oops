41ead00c50c65bf153fda9d8eca336d5c250d480
Anmol <anmolkumar58575@gmail.com>
2024-02-02 16:52:47 +0530

    abstarct class

--- index.php
+++ index.php
@@ -321,8 +321,103 @@ echo '<br>';
 // $strawberry->intro(); // intro() is protected
 echo '<br>';
 
+// class constants are case-sensitive. However, it is recommended to name the constants in all uppercase letters.
+
+// We can access a constant from outside the class by using the class name followed by the scope resolution operator (::) followed by the constant name, like here:
+
+class Goodbye {
+  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+}
+echo '<br>';
+echo Goodbye::LEAVING_MESSAGE;
+echo '<br>';
 
 
+// we can access a constant from inside the class by using the self keyword followed by the scope resolution operator (::) followed by the constant name, like here:
+class Goodbye2 {
+  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+  public function byebye() {
+    echo self::LEAVING_MESSAGE;
+  }
+}
+echo '<br>';
+$goodbye = new Goodbye2();
+echo '<br>';
+$goodbye->byebye();
+
+
+
+
+// Abstract classes and methods are when the parent class has a named method, but need its child class(es) to fill out the tasks.
+
+// An abstract class is a class that contains at least one abstract method. An abstract method is a method that is declared, but not implemented in the code.
+
+// An abstract class or method is defined with the abstract keyword:
+
+// Parent class
+abstract class Car {
+  public $name;
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  abstract public function intro() : string;
+}
+
+// Child classes
+class Audi extends Car {
+  public function intro() : string {
+    return "Choose German quality! I'm an $this->name!";
+  }
+}
+
+class Volvo extends Car {
+  public function intro() : string {
+    return "Proud to be Swedish! I'm a $this->name!";
+  }
+}
+
+class Citroen extends Car {
+  public function intro() : string {
+    return "French extravagance! I'm a $this->name!";
+  }
+}
+
+// Create objects from the child classes
+$audi = new audi("Audi");
+echo $audi->intro();
+echo "<br>";
+
+$volvo = new volvo("Volvo");
+echo $volvo->intro();
+echo "<br>";
+
+$citroen = new citroen("Citroen");
+echo $citroen->intro();
+
+
+// example 2 
+abstract class ParentClass {
+  // Abstract method with an argument
+  abstract protected function prefixName($name);
+}
+
+class ChildClass extends ParentClass {
+  public function prefixName($name) {
+    if ($name == "John Doe") {
+      $prefix = "Mr.";
+    } elseif ($name == "Jane Doe") {
+      $prefix = "Mrs.";
+    } else {
+      $prefix = "";
+    }
+    return "{$prefix} {$name}";
+  }
+}
+
+$class = new ChildClass;
+echo $class->prefixName("John Doe");
+echo "<br>";
+echo $class->prefixName("Jane Doe");
 
 
 
